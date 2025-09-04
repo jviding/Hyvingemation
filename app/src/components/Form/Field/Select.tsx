d4e7fe1bcb07d53b31c12b2be74860890a1c5f12
Jasu Viding <jasu.viding@gmail.com>
2025-09-04 04:01:59 +0300

    add value to set default option

--- app/src/components/Form/Field/Select.tsx
+++ app/src/components/Form/Field/Select.tsx
@@ -2,13 +2,21 @@ import React from "react";
 
 export type SelectFieldProps = {
   name: string;
-  label: string;
+  label?: string;
+  value?: string;
   options: { label: string; value: string }[];
   required?: boolean;
   onChange: (name: string, value: string) => void;
 };
 
-export function SelectField({ name, label, options, required, onChange }: SelectFieldProps) {
+export function SelectField({ 
+  name, 
+  label,
+  value,
+  options, 
+  required = false, 
+  onChange 
+}: SelectFieldProps) {
   return (
     <div>
       <label htmlFor={name} className="block text-sm font-medium text-gray-700">
@@ -17,6 +25,7 @@ export function SelectField({ name, label, options, required, onChange }: Select
       <select
         id={name}
         name={name}
+        value={value}
         required={required}
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) => onChange(name, e.target.value)}
         className="mt-1 block w-full border border-gray-300 bg-white rounded px-3 py-2"
