ca3580649b58c921052088269b464c4eec49af67
Jasu Viding <jasu.viding@gmail.com>
2025-09-05 04:22:34 +0300

    fix response types

--- app/src/app/api/admin/users/route.tsx
+++ app/src/app/api/admin/users/route.tsx
@@ -8,23 +8,23 @@ import { handleRequest } from '@/lib/api/middleware';
 
 export const GET = handleRequest(async (request: Request) => {
     const users = await prisma.user.findMany();
-    return NextResponse.json(users);
+    return NextResponse.json(users, { status: 200 });
 })
 
 export const POST = handleRequest(CreateUserSchema, async (request: Request, data: CreateUser) => {
   const user = await createUser(data);
   console.log('Created user:', user);
-  return NextResponse.json({}, { status: 201 });
+  return new NextResponse(null, { status: 201 });
 })
 
 export const PUT = handleRequest(UpdateUserSchema, async (request: Request, data: UpdateUser) => {
     const user = await updateUser(data);
     console.log('Modified user:', user);
-    return NextResponse.json({}, { status: 204 });
+    return new NextResponse(null, { status: 204 });
 })
 
 export const DELETE = handleRequest(DeleteUserSchema, async (request: Request, data: DeleteUser) => {
     const user = await deleteUser(data);
     console.log('Deleted user:', user);
-    return NextResponse.json({}, { status: 204 });
+    return new NextResponse(null, { status: 204 });
 })
