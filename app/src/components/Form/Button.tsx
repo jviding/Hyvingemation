c4ee19c91820551e1f3e231d69a6a0310d740a76
Jasu Viding <jasu.viding@gmail.com>
2025-09-09 22:46:27 +0300

    add color: red

--- app/src/components/Form/Button.tsx
+++ app/src/components/Form/Button.tsx
@@ -5,7 +5,7 @@ import React from "react";
 type ButtonProps = {
   label: string;
   type?: "button" | "submit" | "reset";
-  color?: "gray" | "blue";
+  color?: "gray" | "blue" | "red";
   onClick?: (e: React.FormEvent) => void;
   size?: string;
 };
@@ -21,9 +21,11 @@ export function Button({
   const getClassName = (color?: string) => {
     switch(color) {
       case "blue":
-        return "bg-blue-600 text-white rounded hover:bg-blue-700"; // px-4 py-2
+        return "bg-blue-600 text-white rounded hover:bg-blue-700";
+      case "red":
+        return "bg-red-600 text-white rounded hover:bg-red-700"; // font-medium
       default:
-        return "bg-gray-300 text-black rounded hover:bg-gray-400"; // px-4 py-2
+        return "bg-gray-300 text-black rounded hover:bg-gray-400";
     };
   }
 
